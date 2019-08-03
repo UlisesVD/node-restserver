d6eee7d0f544698c663020c20d7b16c73749b0a7
Ulises Vera Delgado <uli_vra@hotmail.com>
2019-08-03 18:31:18 -0500

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,26 @@
+// ======================
+// PUERTO
+// ======================
+
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================
+// Produccion
+// ======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ======================
+// Base datos
+// ======================
+
+let urlBD;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb+srv://mongo:mongo123@nodedb-8zgim.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlBD
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -8,33 +9,12 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
- 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-})
+// routes of the app
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Nombre necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        });    
-    }
-    
-})
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({id});
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
-})
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true}, (err, res) => {
+    if (err) throw err;
+    console.log('Conexion a bd realizada');
+});
 
 app.listen(process.env.PORT, ()=> console.log(`Escuchando en puerto ${process.env.PORT}`))
